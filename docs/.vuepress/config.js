b3f4902f83003e5b6371cffb4fd9d0fbcb539a02
strugglinglee <strugglinglee@163.com>
2022-04-23 23:55:38 +0800

    docs: BFS

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: "算法学习",
-  description: "Just playing around",
-  base: "/algorithm-learn/",
+  title: '算法学习',
+  description: 'Just playing around',
+  base: '/algorithm-learn/',
   configureWebpack: {
     resolve: {
       alias: {
@@ -11,57 +11,61 @@ module.exports = {
   },
   // theme: 'reco',
   locales: {
-    "/": {
-      lang: "zh-CN",
+    '/': {
+      lang: 'zh-CN',
     },
   },
   themeConfig: {
-    subSidebar: "auto",
+    subSidebar: 'auto',
     nav: [
-      { text: "首页", link: "/" },
+      { text: '首页', link: '/' },
       {
-        text: "了解更多",
+        text: '了解更多',
         items: [
-          { text: "Github", link: "https://github.com/strugglinglee" },
+          { text: 'Github', link: 'https://github.com/strugglinglee' },
           {
-            text: "博客",
-            link: "https://blog.strugglinglee.cn/",
+            text: '博客',
+            link: 'https://blog.strugglinglee.cn/',
           },
         ],
       },
     ],
     sidebar: [
       {
-        title: "数据结构",
-        path: "/数据结构.md",
+        title: '数据结构',
+        path: '/数据结构.md',
       },
       {
-        title: "数组的应用",
-        path: "/数组的应用.md",
+        title: '数组的应用',
+        path: '/数组的应用.md',
       },
       {
-        title: "字符串应用",
-        path: "/字符串应用.md",
+        title: '字符串应用',
+        path: '/字符串应用.md',
       },
       {
-        title: "hash表",
-        path: "/hash表.md",
+        title: 'hash表',
+        path: '/hash表.md',
       },
       {
-        title: "链表",
-        path: "/list-node/base.md",
+        title: '链表',
+        path: '/list-node/base.md',
       },
       {
-        title: "时间复杂度和空间复杂度",
-        path: "/时间复杂度和空间复杂度.md",
+        title: '时间复杂度和空间复杂度',
+        path: '/时间复杂度和空间复杂度.md',
       },
       {
-        title: "递归",
-        path: "/递归/base.md",
+        title: '递归',
+        path: '/递归/base.md',
       },
       {
-        title: "二分法",
-        path: "/二分法/base.md",
+        title: '二分法',
+        path: '/二分法/base.md',
+      },
+      {
+        title: '广度优先搜索',
+        path: '/BFS/BFS.md',
       },
     ],
   },
