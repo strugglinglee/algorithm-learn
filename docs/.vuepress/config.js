2b2915f42145a2f0e5c13188b2ec4fed36f58b70
strugglinglee <strugglinglee@163.com>
2022-05-25 13:36:41 +0800

    docs: list-node

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,7 +49,16 @@ module.exports = {
       },
       {
         title: "链表",
-        path: "/list-node/base.md",
+        children: [
+          {
+            title: "链表基础",
+            path: "/list-node/base.md",
+          },
+          {
+            title: "算法 | 链表的处理-链表的合并及删除",
+            path: "/list-node/list-instance1.md",
+          },
+        ],
       },
       {
         title: "时间复杂度和空间复杂度",
